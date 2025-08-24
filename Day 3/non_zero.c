3567d3543fe31aaf011f373b8cfe3a67cc5b72d1
Sindhu Uday Naik <sindhuudaynaik@gmail.com>
2025-08-24 18:52:04 +0530

    Create non_zero.c
    non zero

---
+++ Day 3/non_zero.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    if (1){
+        printf("It is 1\n");
+    }
+    if (2345){
+        printf("It is 2345\n");
+    }
+    if (2.43)
+    {
+        printf("It is 2.43\n");
+    }
+    if ('c')
+    {
+        printf("It is c\n");
+    }
+    if (0)
+    {
+        printf("It is 0\n");
+    }
+    
+    
+}
